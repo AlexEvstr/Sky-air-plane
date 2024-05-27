46d0692da59dabdbbc5dd3e9956925719a242ffe
Alexander Evstratov <alexdrugev@me.com>
2024-05-27 12:21:43 +0300

    Add background movement.

---
+++ Assets/_Scripts/BackgroundMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    private float movementSpeed = 2.5f;
+    private Vector2 _startPosition;
+    private float _backgroundCenter;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _backgroundCenter = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);
+        if (transform.position.x < (_startPosition.x - _backgroundCenter)) transform.position = _startPosition;
+    }
+}
