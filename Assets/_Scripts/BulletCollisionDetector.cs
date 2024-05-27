8fbd863fc35590e5b795b45f4343c3fe18c507b7
Alexander Evstratov <alexdrugev@me.com>
2024-05-27 18:25:05 +0300

    Add bullet collision.

---
+++ Assets/_Scripts/BulletCollisionDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollisionDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
