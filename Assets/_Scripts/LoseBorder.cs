58aaffbbf9b938e7a396dcd88a741bde7fc868c1
Alexander Evstratov <alexdrugev@me.com>
2024-05-29 18:30:44 +0300

    Add shield bonus.

--- Assets/_Scripts/LoseBorder.cs
+++ Assets/_Scripts/LoseBorder.cs
@@ -6,24 +6,35 @@ public class LoseBorder : MonoBehaviour
     [SerializeField] private GameObject _losePanel;
     [SerializeField] private Button _firstKitBtn;
     [SerializeField] private GameObject _useBonus;
+    [SerializeField] private GameObject _shield;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Plane"))
         {
-            _losePanel.SetActive(true);
-            int _kitCount = PlayerPrefs.GetInt("FirstAidKit", 0);
-            if (_kitCount > 0)
+            if (!_shield.activeInHierarchy)
             {
-                _firstKitBtn.enabled = true;
-                _useBonus.SetActive(true);
+                _losePanel.SetActive(true);
+                int _kitCount = PlayerPrefs.GetInt("FirstAidKit", 0);
+                if (_kitCount > 0)
+                {
+                    _firstKitBtn.enabled = true;
+                    _useBonus.SetActive(true);
+                }
+                else
+                {
+                    _firstKitBtn.enabled = false;
+                    _useBonus.SetActive(false);
+
+                }
             }
             else
             {
-                _firstKitBtn.enabled = false;
-                _useBonus.SetActive(false);
-
+                _shield.SetActive(false);
+                Destroy(collision.gameObject);
+                PlaneSpawner.CanSpawnPlane = true;
             }
+            
         }
     }
 }
--- Assets/_Scripts/ShieldBtnGame.cs
+++ Assets/_Scripts/ShieldBtnGame.cs
@@ -1,9 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShieldBtnGame : MonoBehaviour
 {
     [SerializeField] private TMP_Text _shieldCountText;
+    [SerializeField] private GameObject _shield;
+    [SerializeField] private GameObject _lose;
+    [SerializeField] private GameObject _pause;
+    [SerializeField] private GameObject _win;
     private int _shieldCount;
 
     private void Start()
@@ -14,11 +19,29 @@ public class ShieldBtnGame : MonoBehaviour
 
     public void UseShield()
     {
-        if (_shieldCount > 0)
+        if (_shieldCount > 0 && !_shield.activeInHierarchy)
         {
             _shieldCount--;
             PlayerPrefs.SetInt("Shield", _shieldCount);
             _shieldCountText.text = _shieldCount.ToString();
+            ShieldBehavior();
+        }
+    }
+
+    private void ShieldBehavior()
+    {
+        _shield.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (_lose.activeInHierarchy || _pause.activeInHierarchy || _win.activeInHierarchy)
+        {
+            gameObject.GetComponent<Button>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<Button>().enabled = true;
         }
     }
 }
