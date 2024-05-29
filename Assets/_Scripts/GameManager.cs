557e0eddb3bc08a6491d06fc5ddfba4c5ff14a3b
Alexander Evstratov <alexdrugev@me.com>
2024-05-29 19:59:56 +0300

    Change all UI.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _levelCompleteText;
     [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private GameObject _pauseBtn;
     public static int CurrentLevel;
 
     private void Start()
@@ -18,12 +19,14 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         Time.timeScale = 0;
+        _pauseBtn.SetActive(false);
         _pausePanel.SetActive(true);
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1;
+        _pauseBtn.SetActive(true);
         _pausePanel.SetActive(false);
     }
 
