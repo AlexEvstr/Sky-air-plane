487f4b788dc455c3fac3d254a1cf840fffb373ec
Alexander Evstratov <alexdrugev@me.com>
2024-05-30 00:47:26 +0300

    Add onboard.

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -7,12 +7,19 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject _privacyWindow;
     [SerializeField] private GameObject _tutorialWindow;
     [SerializeField] private TMP_Text _coinsText;
+    [SerializeField] private GameObject _onboard_1;
+    [SerializeField] private GameObject _onboard_2;
+    [SerializeField] private GameObject _onboard_3;
 
     public static int CoinsCount;
 
     private void Start()
     {
         Time.timeScale = 1;
+        if (PlayerPrefs.GetString("EnterIsNotFirst", "") == "")
+        {
+            _onboard_1.SetActive(true);
+        }
         CoinsCount = PlayerPrefs.GetInt("Coins", 0);
         _coinsText.text = CoinsCount.ToString();
     }
@@ -51,4 +58,23 @@ public class MainMenuController : MonoBehaviour
     {
         _coinsText.text = CoinsCount.ToString();
     }
+
+    public void Goto2()
+    {
+        _onboard_1.SetActive(false);
+        _onboard_2.SetActive(true);
+    }
+
+    public void Goto3()
+    {
+        _onboard_2.SetActive(false);
+        _onboard_3.SetActive(true);
+    }
+
+    public void GotoGame()
+    {
+        _onboard_3.SetActive(false);
+        PlayerPrefs.SetString("EnterIsNotFirst", "really");
+    }
+
 }
